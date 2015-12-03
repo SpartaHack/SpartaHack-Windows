2888f295b5efbc590a7091f89fc6cd1f914f2607
Matt Pasco <matt.pasco@hotmail.com>
2015-12-03 17:35:52 -0500

    Added announcements

--- SpartaHack/SpartaHack/HomePage.xaml.cs
+++ SpartaHack/SpartaHack/HomePage.xaml.cs
@@ -12,7 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using Parse;
 namespace SpartaHack
 {
     public sealed partial class HomePage : Page
@@ -20,9 +20,30 @@ namespace SpartaHack
         public HomePage()
         {
             this.InitializeComponent();
-            List<string> data = new List<string>() { "This is a Test", "Blah", "Something else" };
-            Data.Source = data;
+            getAnnouncements();
+           
             
+        }
+        async void getAnnouncements()
+        {
+            //try
+            //{
+                List<Announcement> announcements = new List<Announcement>();
+                List<ParseObject> _announcements = new List<ParseObject>(await ParseObject.GetQuery("Announcements").FindAsync());
+                foreach(ParseObject obj in _announcements)
+                {
+                    announcements.Add(new Announcement
+                    {
+                        Title = obj["Title"].ToString(),
+                        Description = obj["Description"].ToString()
+                    });
+                }
+                Data.Source = announcements;
+            //}
+            //catch
+            //{
+
+            //}
         }
         protected override  void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -30,5 +51,10 @@ namespace SpartaHack
             MainPage.title.Value = "NOTIFICATIONS";
         }
 
+    }
+    class Announcement
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
     }
 }
